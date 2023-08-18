cd680f4a4cbcf616316fb27984e4ff0c0c8ed9e9
Deepkamal yadav <dkits1995@gmail.com>
2023-08-18 21:20:58 +0530

    graphQl hello world

---
+++ fakedb.js
@@ -0,0 +1,37 @@
+// collection/table -users
+//                    -doc1
+//                    -doc2
+
+export const users = [
+  {
+    id: "12345",
+    name: "John",
+    lastName: "yadav",
+    email: "johndoe@gmail.com",
+    password: "12345",
+  },
+  {
+    id: "123456",
+    name: "Deep",
+    lastName: "singh",
+    email: "deep@gmail.com",
+    password: "123456",
+  },
+];
+
+/* collection/table -Quotes
+                        -doc1
+                       -doc2
+                        etc
+                        */
+
+export const quotes = [
+  {
+    name: "I turn Coffee into code",
+    by: "12345",
+  },
+  {
+    name: "If it works don't touch it",
+    by: "123456",
+  },
+];
---
+++ index.js
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+const resolvers = {
+  Query: {
+    greet: () => "HELLO WORLD",
+  },
+};
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`serve ready on ${url}`);
+});
